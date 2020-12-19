6a1054598f695fb7ccc5b26270e838a61c738ba7
Nabby12 <Nabby12@users.noreply.github.com>
2020-12-20 05:56:33 +0900

    added/test-code-for-handler

--- __tests__/unit/handlers/test.js
+++ __tests__/unit/handlers/test.js
@@ -1,17 +1,77 @@
 'use strict'
 
-const fs = require('fs');
+const sinon = require('sinon');
 const chai = require('chai');
+chai.use(require('chai-as-promised'))
 const assert = chai.assert;
-const lambda = require('../../../src/handlers/index.js');
+const expect = chai.expect;
 
-async function getEnv() {
-    const envData = await JSON.parse(fs.readFileSync('.../../env.json'));
-    return envData.insertDynamoDb;
-};
-describe('Test for index', async () => {
-    const env = await getEnv();
-    it('DynamoDBにデータが追加されるか', async () => {
-        console.log(env.DYNAMO_TABLE_NAME);
+const lambda = require('../../../src/handlers/index.js');
+const googleSheetsModule = require('../../../src/handlers/googleSheetsModule');
+const dynamoDbModule = require('../../../src/handlers/dynamoDbModule');
+
+describe('Test for index', () => {
+    let dynamoDBPutStub;
+    let dynamoDBScanStub;
+    let getSpreadSheetPhraseStub;
+    
+    beforeEach(function() {
+        dynamoDBScanStub = sinon.stub(dynamoDbModule, 'scanDynamo');
+        dynamoDBPutStub = sinon.stub(dynamoDbModule, 'putDynamo');
+        getSpreadSheetPhraseStub = sinon.stub(googleSheetsModule, 'getSpreadSheetPhrase');
+    });
+
+    afterEach(function() {
+        dynamoDBScanStub.restore();
+        dynamoDBPutStub.restore();
+        getSpreadSheetPhraseStub.restore();
+    });
+    
+    it('DynamoDBにデータ追加できた場合に正常終了のステータスが返る', async () => { 
+        getSpreadSheetPhraseStub.returns(Promise.resolve([
+            'テストデータ',
+            'test data'
+        ]));
+        dynamoDBScanStub.returns(Promise.resolve(1));       
+        dynamoDBPutStub.returns(Promise.resolve({'isOk': true}));       
+
+        return expect(lambda.handler()).to.be.fulfilled.then(result => {
+            assert.equal(dynamoDBScanStub.calledOnce, true);
+            assert.equal(dynamoDBPutStub.called, true);
+            assert.equal(getSpreadSheetPhraseStub.calledOnce, true);
+            assert.deepEqual(result, { status: 'put item succeeded.' });
+        });
+    });
+
+    it('DynamoDBにデータ追加できなかった場合に失敗ステータスが返る', async () => {        
+        getSpreadSheetPhraseStub.returns(Promise.resolve([
+            'テストデータ',
+            'test data'
+        ]));
+        dynamoDBScanStub.returns(Promise.resolve(1));       
+        dynamoDBPutStub.returns(Promise.resolve({'isOk': false}));       
+
+        return expect(lambda.handler()).to.be.fulfilled.then(result => {
+            assert.equal(dynamoDBScanStub.calledOnce, true);
+            assert.equal(dynamoDBPutStub.called, true);
+            assert.equal(getSpreadSheetPhraseStub.calledOnce, true);
+            assert.deepEqual(result, { status: 'put item failed.' });
+        });
+    });
+
+    it('DynamoDBのレコード数が取得できない場合に失敗ステータスが返る', async () => {        
+        getSpreadSheetPhraseStub.returns(Promise.resolve([
+            'テストデータ',
+            'test data'
+        ]));
+        dynamoDBScanStub.returns(Promise.resolve(-1));       
+        dynamoDBPutStub.returns(Promise.resolve({'isOk': true}));       
+
+        return expect(lambda.handler()).to.be.fulfilled.then(result => {
+            assert.equal(dynamoDBScanStub.calledOnce, true);
+            assert.equal(dynamoDBPutStub.called, false);
+            assert.equal(getSpreadSheetPhraseStub.calledOnce, false);
+            assert.deepEqual(result, { status: 'db scan failed.' });
+        });
     });
 });
